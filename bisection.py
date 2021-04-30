5bed1b1427b389cfc2ff30ca8cf7afd3a7c8c9c9
aakgn <75807351+aakgn@users.noreply.github.com>
2021-04-30 14:42:17 +0300

    Add files via upload

--- bisection.py
+++ bisection.py
@@ -9,7 +9,7 @@
 # Two dimensional root finding !!!
 # @bugs:
 
-
+from math import *
 class Bisection:
     
     def __init__ (self, x1, x2, N, error_margin, function):
@@ -19,30 +19,34 @@ class Bisection:
         self.error_margin = error_margin
         self.function = function
         
-    def broot (self, x1, x2, N, error_margin):
+    def broot(self, x1, x2, N, error_margin):
+        
         for i in range(0, N):
             
             x = (x1 + x2) / 2
             
-            if (function(x1) * function(x2) > 0):
+            if (function(x2) * function(x) > 0):
                 x2 = x         
             else:
-               x2 = x
+                x1 = x
             
-            if (abs(function(x) < error_margin)):
+            if (abs(function(x)) < error_margin):
                 break
+            
+            if (i == N - 1):
+                x = -1
         
         return x
 
 
 
 error_margin = 1e-7
-x1 = 1
-x2 = 10
+x1 = 0
+x2 = 100
 N = 1000
 
 
-user_input = "x*x - 4"
-function = lambda x: eval(user_input)
+function_input = "x * x - 9"
+function = lambda x: eval(function_input)
 root1 = Bisection(x1, x2, N, error_margin, function)
 print(root1.broot(x1, x2, N, error_margin))
